7b5cd852bf50c42863ee47d324b10aeff3afad6e
Taylor Somma <taylorsomma@taylors-air.attlocal.net>
2014-11-02 06:30:39 -0500

    UIPickerView for add semester view controller is working

--- MakeTheGrade/Course.swift
+++ MakeTheGrade/Course.swift
@@ -11,7 +11,53 @@ import CoreData
 
 class Course: NSManagedObject {
 
+    @NSManaged var courseTitle: String
+    @NSManaged var credits: NSNumber
+    @NSManaged var examsPerc: NSNumber
+    @NSManaged var gradeOverride: NSNumber
+    @NSManaged var homeworkPerc: NSNumber
+    @NSManaged var otherPerc: NSNumber
     @NSManaged var pointsOrPercentage: NSNumber
-    @NSManaged var gradeList: NSManagedObject
+    @NSManaged var quizesPerc: NSNumber
+    @NSManaged var scienceCourse: NSNumber
+    @NSManaged var semesterSeason: String
+    @NSManaged var belongsTo: Semester
+    @NSManaged var gradeList: NSSet
 
+    // change [Int] to [Grade]
+    func getGradeList() -> [Int]
+    {
+        //return [Grade(entity: NSEntityDescription, insertIntoManagedObjectContext: <#NSManagedObjectContext!#>)]
+        return [10]
+    }
+    
+    func getCourseAverage() -> Float
+    {
+        return 100.0
+    }
+    
+    func letterGrade() -> String
+    {
+        var points = self.getCourseAverage()
+        var returnLetter = "Invalid"
+        if (points > 93)
+        {
+            returnLetter = "A"
+        }
+        else if (points >= 90)
+        {
+            returnLetter = "A-"
+        }
+        else if (points >= 87)
+        {
+            returnLetter = "B+"
+        }
+        else if (points >= 83)
+        {
+            returnLetter = "B"
+        }
+        
+        return returnLetter
+        
+    }
 }
--- MakeTheGrade/NewSemesterViewController.swift
+++ MakeTheGrade/NewSemesterViewController.swift
@@ -9,7 +9,42 @@
 import Foundation
 import UIKit
 
-class NewSemesterViewController: UIViewController {
+class NewSemesterViewController: UIViewController, UIPickerViewDataSource {
+    @IBOutlet var semesterPickerView: UIPickerView!
+    var semesters = ["Fall","Spring","Winter","Summer"]
+    var years = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018]
+    
+    // returns the number of 'columns' to display.
+    func numberOfComponentsInPickerView(semesterPickerView: UIPickerView) -> Int
+    {
+        return 2
+    }
+    
+    // returns the # of rows in each component..
+    func pickerView(semesterPickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        if (component==0)
+        {
+            return semesters.count
+        }
+        else
+        {
+            return years.count
+        }
+    }
+    
+    func pickerView(semesterPickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String!
+    {
+        if (component==0)
+        {
+            return semesters[row]
+        }
+        else
+        {
+            return String(years[row])
+        }
+    }
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
